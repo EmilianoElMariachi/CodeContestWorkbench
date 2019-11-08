6d36a405db4d0f07e307afc4bdeef33962f0e446
Emilien NATTER <emilien.natter@socgen.com>
2019-11-08 10:43:55 +0100

    Add default example for CodingGame

--- CodingGame/code.js
+++ CodingGame/code.js
@@ -1,8 +1,1 @@
-N= +(readline());
-l=[]
-t=N
-print("#".repeat(N))
-while(t--){
-l.push(" ".repeat(N-t-1)+"#".repeat(t*2+1))
-}
-l.slice().reverse().concat(l).forEach(e=>print(e))
+print(parseInt(readline())*3)
