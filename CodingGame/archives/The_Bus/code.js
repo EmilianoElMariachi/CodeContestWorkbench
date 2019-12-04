d61f18b4622ff19baef60ffe01e5624a9af63af2
Emilien NATTER <emilien.natter@socgen.com>
2019-12-04 19:18:39 +0100

    Bus example added

---
+++ CodingGame/archives/The_Bus/code.js
@@ -0,0 +1,25 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N = parseInt(readline());
+const inputs = readline().split(' ');
+let stops=[]
+for (let i = 0; i < N; i++) {
+    let nbPersEntering=parseInt(inputs[i]);
+    stops.push(nbPersEntering);
+}
+let nbStayPerPers=readline().split(" ");
+
+
+stops.forEach((nbPersEntering, stopIndex) => {
+
+
+});
+
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log("To be finished");
