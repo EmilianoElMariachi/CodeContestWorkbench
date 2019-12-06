264a1d7e153f2fed1737c0570c41938bd37680a9
EmilianoElMariachi <emignatik@gmail.com>
2019-12-06 21:46:05 +0100

    Finalize impl of Sum_Product

--- CodingGame/archives/Sum_Product/code.js
+++ CodingGame/archives/Sum_Product/code.js
@@ -1,12 +1,4 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-const S = parseInt(readline());
-const P = parseInt(readline());
-
-// Write an action using console.log()
-// To debug: console.error('Debug messages...');
-
-console.log("TODO");
+r=readline
+S=r()
+b=(-S-Math.sqrt(S*S-4*r()))/-2
+print(a=S-b+" "+b)
